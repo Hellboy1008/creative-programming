34b535cad0eb7783121911d237cbe66f585f18ff
Stephen Lee <srlee@ucsd.edu>
2020-09-25 23:52:38 +0900

    use js for viewport size

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,13 @@
 // run these functions when the website is loaded
 $(document).ready(function() {
+    // smooth scroll
     $('.scroll-down').click(function(event) {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top - $('.navbar').height()
         }, 500);
         return false;
     });
+    // change height of picture depending on viewport
+    $('.site-overlay').css('height', window.innerHeight);
+    $('.welcome-screen').css('height', window.innerHeight);
 });
