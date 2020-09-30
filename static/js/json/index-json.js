92b64c1cccccac41b5424e7322992d9a20a11825
Stephen Lee <srlee@ucsd.edu>
2020-10-01 01:55:11 +0900

    edit theme for the year

--- static/js/json/index-json.js
+++ static/js/json/index-json.js
@@ -7,9 +7,9 @@
 var index_text = {
     mission_body: "The mission of Asian American Christian Fellowship is to reach into the university and collegiate community, primarily to those who are Asian Pacific Americans, with the life-changing message of Jesus Christ.",
     mission_header: "Mission Statement",
-    theme_header: "2019-2020 Theme: Present",
-    theme_verse_body: "I appeal to you therefore, brothers, by the mercies of God, to present your bodies as a living sacrifice, holy and acceptable to God, which is your spiritual worship. Do not be conformed to this world, but be transformed by the renewal of your mind, that by testing you may discern what is the will of God, what is good and acceptable and perfect.",
-    theme_verse_source: "Romans 12:1-2 (ESV)",
+    theme_header: "2020-2021 Theme: ",
+    theme_verse_body: "Going to be announced at our first large group!",
+    theme_verse_source: "",
     vision_body: "Our vision of AACF is to be committed to making disciples by educating and equipping students in biblical foundations, Christian disciplines, and the development of spiritual gifts in order to prepare them for witness and service on the campus, in the local church, in the marketplace, and in world missions.",
     vision_header: "Vision",
     welcome_image: "images/Front_Page/Brothers_2020.jpg",
